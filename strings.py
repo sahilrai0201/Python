dd3aada6625abfa6c38153fa6ac1ed845b036b92
sahilrai0201 <sahilatwork21@gmail.com>
2026-04-02 00:30:01 +0530

    Strings in Python

---
+++ strings.py
@@ -0,0 +1,16 @@
+#----------------------------------------STRINGS-----------------------------------------------
+# str1 = "This is a string."
+# str2 = 'ApnaCollege'
+# str3 = """Example string"""
+
+# str4 = "This is a string.\n We are creating it in Python."
+# print(str4)
+
+#concatenation
+str1 = "Apna"
+str2 = "College"
+print(str1 + " " + str2)
+
+#len() is used to find the length of a string
+print(len(str1))
+print(len(str2))
